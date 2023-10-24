8de0707b4fb4465eb76e0570a26565e0d5d8e604
Alex666000 <bogdanov777000@mail.ru>
2023-10-24 21:06:48 +0300

    route auth

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,22 +1,45 @@
 import { IoMdArrowBack } from 'react-icons/io'
-import { useAuth } from '../../../hooks/useAuth.js'
-import Hamburger from '../hamburger/Hamburger.jsx'
+import { SlUser } from 'react-icons/sl'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../../hooks/useAuth'
+import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
 
-const Header = ({ backLink }) => {
-	/* TODO: React router useHistory */
+const Header = ({ backLink = '' }) => {
+	const { pathname } = useLocation()
+	const navigate = useNavigate()
 
 	const { isAuth } = useAuth()
 
 	return (
 		<header className={styles.header}>
-			<button>
-				<IoMdArrowBack />
-			</button>
-			{/* User profile */}
+			{pathname !== '/' ? (
+				// путь по которому когда открыто приложение – если у нас
+				// не главная страница, а другая то отображаем
+				// стрелку назад иначе отобразим кнопку профиля
+				<button
+					onClick={() => {
+						navigate(backLink)
+					}}
+				>
+					<IoMdArrowBack fill='#fff' fontSize={29} />
+				</button>
+			) : (
+				<button
+					onClick={() => {
+						navigate('/profile')
+					}}
+				>
+					<SlUser fill='#fff' fontSize={25} /> {/* Иконка профиля */}
+				</button>
+			)}
 			<Hamburger />
 		</header>
 	)
 }
 
 export default Header
+
+/*
+Логика редиректа с профиля на главную и обратно
+ */
