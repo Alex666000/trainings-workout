a0261e65533c640ead02c92f9e26b72571842c02
Alex666000 <bogdanov777000@mail.ru>
2023-10-26 13:25:28 +0300

    add react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import ReactDOM from "react-dom/client"
-
-import "./assets/styles/index.scss"
 import Router from "./routes/Routes"
+import "./assets/styles/index.scss"
 
 const queryClient = new QueryClient({
 	defaultOptions: {
